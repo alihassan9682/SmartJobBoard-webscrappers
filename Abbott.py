3179b63741b700667691f16d754eb586bd4a7aa6
meibrarhussain <ibrarhussain0602@gmail.com>
2024-08-22 03:03:27 +0500

    fixed the Boston Abbott

--- Abbott.py
+++ Abbott.py
@@ -7,10 +7,39 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
+from extractCityState import find_city_state_in_title
 from helpers import configure_webdriver, is_remote  # Ensure these imports are correct for your environment
 
 def request_url(driver, url):
     driver.get(url)
+
+def filter_job_title(job_title):
+    valid_titles = [
+        "Specialist",
+        "Speciality",
+        "Representative",
+        "District Manager",
+        "Regional manager",
+        "Account Manager",
+        "Sales Manager",
+        "Sales Director",
+        "Account Executive",
+        "District Manager",
+        "Regional Manager",
+        "Account Manager",
+        "Sales Executive",
+        "Regional Director",
+        "Territory Manager",
+        "Account Executive",
+        "Senior Executive",
+        "Client Manager",
+        "Marketing Manager",
+        "Brand Manager"
+    ]
+    for valid_title in valid_titles:
+        if valid_title.lower() in job_title.lower():
+            return True
+    return False
 
 def write_to_csv(data, directory, filename):
     fieldnames = list(data[0].keys())
@@ -75,6 +104,8 @@ def getJobs(driver):
             driver.get(job)
             time.sleep(2)
             jobTitle = driver.find_element(By.CLASS_NAME, "job-title").text
+            if not filter_job_title(jobTitle):
+                continue
             page_source = driver.page_source
             soup = BeautifulSoup(page_source, "html.parser")
             desc_content = soup.find("section", class_="job-description")
@@ -112,6 +143,12 @@ def getJobs(driver):
             else:
                 print('issue with the location')
             Zipcode = ''
+            city_title, state_title = find_city_state_in_title(jobTitle)
+            if city_title:
+                City = city_title
+            if state_title:
+                state = state_title
+
             date_posted = soup.find('div', class_='job-info au-target')['data-ph-at-job-post-date-text']
             job_id = soup.find('div', class_='job-info au-target')['data-ph-at-job-id-text']
 
