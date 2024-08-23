561bc526a6e0b4e67234c27ba94961213a8d2d53
meibrarhussain <ibrarhussain0602@gmail.com>
2024-08-24 00:56:50 +0500

    fixed the Stryker

--- Stryker.py
+++ Stryker.py
@@ -1,5 +1,6 @@
 import time
 
+from extractCityState import find_city_state_in_title
 from helpers import configure_webdriver, configure_undetected_chrome_driver, is_remote
 
 from selenium.webdriver.common.by import By
@@ -11,8 +12,38 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
+
 def request_url(driver, url):
     driver.get(url)
+
+
+def filter_job_title(job_title):
+    valid_titles = [
+        "Specialist",
+        "Speciality",
+        "Representative",
+        "District Manager",
+        "Regional manager",
+        "Account Manager",
+        "Sales Manager",
+        "Sales Director",
+        "Account Executive",
+        "District Manager",
+        "Regional Manager",
+        "Account Manager",
+        "Sales Executive",
+        "Regional Director",
+        "Account Executive",
+        "Senior Executive",
+        "Client Manager",
+        "Marketing Manager",
+        "Brand Manager"
+    ]
+    for valid_title in valid_titles:
+        if valid_title.lower() in job_title.lower():
+            return True
+    return False
+
 
 def write_to_csv(data, directory, filename):
     fieldnames = list(data[0].keys())
@@ -25,22 +56,25 @@ def write_to_csv(data, directory, filename):
         for item in data:
             writer.writerow(item)
 
+
 def loadAllJobs(driver):
     JOBS = []
     wait = WebDriverWait(driver, 10)
-    
+
     while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
         results = wait.until(EC.presence_of_element_located(
-            (By.ID, "widget-jobsearch-results-list")
+            (By.CLASS_NAME, "results-list")
         ))
         jobs = WebDriverWait(results, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "jobTitle"))
+            EC.presence_of_all_elements_located(
+                (By.CLASS_NAME, "results-list__item-title"))
         )
         jobs = [
-                job.find_element(By.TAG_NAME, "a").get_attribute(
-                    "href"
-                )
+            job.get_attribute(
+                "href"
+            )
             for job in jobs
         ]
 
@@ -48,12 +82,17 @@ def loadAllJobs(driver):
             JOBS = JOBS + jobs
 
             next_button = wait.until(
-                EC.presence_of_element_located((By.XPATH, "//li[@class='pagination-li']/a[@aria-label='Go to the next page of results.']"))
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "page-link-next"))
             )
-            
             if next_button:
-                driver.execute_script("arguments[0].click();", next_button)
-                time.sleep(2)
+                check_disabled = next_button.get_attribute("aria-disabled")
+                if check_disabled == "false":
+                    driver.execute_script("arguments[0].click();", next_button)
+                    time.sleep(2)
+                else:
+                    print('No more next button')
+                    break
             else:
                 break
         except:
@@ -62,6 +101,7 @@ def loadAllJobs(driver):
 
     return JOBS
 
+
 def getJobs(driver):
     JOBS = []
     jobs = loadAllJobs(driver)
@@ -69,14 +109,14 @@ def getJobs(driver):
         try:
             driver.get(job)
             time.sleep(2)
-            job_meta = driver.find_element(By.CLASS_NAME, "job-title")
+            job_meta = driver.find_element(By.ID, "job-title")
             jobTitle = job_meta.text if job_meta else ''
+            if not filter_job_title(jobTitle):
+                continue
             page_source = driver.page_source
             soup = BeautifulSoup(page_source, "html.parser")
-            desc_content = soup.find("div", class_="job-description-details")
+            desc_content = soup.find("div", class_="description")
             jobDescription = desc_content.prettify() if desc_content else ""
-            job_details_body = soup.find("div", class_="job-details-body")
-
             # Initialize fields
             Location = City = state = ''
             country = 'US'
@@ -90,70 +130,36 @@ def getJobs(driver):
             to_salary = ''
 
             # Extract primary location
-            primary_location_meta = job_details_body.find("p", class_="job-location")
-            if primary_location_meta:
-                primary_location = primary_location_meta.text.split(": ")[-1].strip()
-                Location = primary_location
+            primary_location_meta = soup.find(
+                "div", class_="job-locations-parsed")
 
             # Extract additional locations
-            additional_locations_meta = job_details_body.find("p", class_="job-addtnl-location")
-            if additional_locations_meta:
-                additional_locations_lists = additional_locations_meta.find_next_siblings("ul", class_="add-loc-list")
-                for loc_list in additional_locations_lists:
-                    loc_items = loc_list.find_all("ul", class_="add-loc-list")
-                    for loc in loc_items:
-                        additional_location = loc.text.strip()
+            if primary_location_meta:
+                loc_items = primary_location_meta.find_all("li")
+                for loc_list in loc_items:
+                        additional_location = loc_list.text.strip()
                         if additional_location:
                             additional_locations.append(additional_location)
 
-            # Combine primary and additional locations
-            all_locations = [primary_location] + additional_locations if primary_location else additional_locations
-
             # Separate the details into Location, City, State, Country
-            if all_locations:
-                Location = ', '.join(all_locations)
-                City = ', '.join([loc.split(', ')[0] for loc in all_locations])
-                state = ', '.join([loc.split(', ')[1] for loc in all_locations if len(loc.split(', ')) > 1])
-                country = ', '.join([loc.split(', ')[-1] for loc in all_locations if len(loc.split(', ')) > 2])
+            if additional_locations:
+                Location = ', '.join(additional_locations)
+                City = ', '.join([loc.split(', ')[0]
+                                 for loc in additional_locations])
+                state = ', '.join(
+                    [loc.split(', ')[1] for loc in additional_locations if len(loc.split(', ')) > 1])
+                country = ', '.join(
+                    [loc.split(', ')[-1] for loc in additional_locations if len(loc.split(', ')) > 2])
 
-            # Extract other details
-            if job_details_body:
-                # Date Posted
-                date_posted_meta = job_details_body.find("p", string=lambda x: x and "Date posted:" in x)
-                if date_posted_meta:
-                    posted_date = date_posted_meta.text.split(": ")[-1].strip()
-
-                # Job ID
-                job_id_meta = job_details_body.find("p", string=lambda x: x and "Job ID:" in x)
-                if job_id_meta:
-                    job_id = job_id_meta.text.split(": ")[-1].strip()
-
-                employee_type_meta = job_details_body.find("p", string=lambda x: x and "Employee type:" in x)
-                job_type = employee_type_meta.text.split(": ")[-1].strip() if employee_type_meta else ""
-
-                # Hidden elements
-                city_meta = job_details_body.find("p", class_="job-city hide")
-                City = city_meta.text.strip() if city_meta else City
-
-                state_meta = job_details_body.find("p", class_="job-state hide")
-                state = state_meta.text.strip() if state_meta else state
-
-                country_meta = job_details_body.find("p", class_="job-country hide")
-                country = country_meta.text.strip() if country_meta else country
-
-                address_meta = job_details_body.find("p", class_="job-address hide")
-                Address = address_meta.text.strip() if address_meta else Location
-
-            # Extract salary details
-            salary_meta = soup.find("span", class_="job-salary-range job-info")
-            if salary_meta and "-" in salary_meta.text:
-                salary_range = salary_meta.text.split(" - ")
-                from_salary = salary_range[0].strip()
-                to_salary = salary_range[1].strip()
+            city_title, state_title = find_city_state_in_title(jobTitle)
+            if city_title:
+                City = city_title
+            if state_title:
+                state = state_title
 
             # Construct job details dictionary
             jobDetails = {
-                "Job Id": job_id,
+                "Job Id": job_id if job_id else jobs.index(job),
                 "Job Title": jobTitle,
                 "Job Description": jobDescription,
                 "Job Type": job_type,
@@ -163,7 +169,7 @@ def getJobs(driver):
                 "State": state,
                 "Country": country,
                 "Zip Code": Zipcode,
-                "Address": Address,
+                "Address": Location,
                 "Remote": is_remote(Location),
                 "Salary From": from_salary,
                 "Salary To": to_salary,
@@ -180,8 +186,8 @@ def getJobs(driver):
                 "Company Name": "Stryker",
                 "Employer Website": "https://careers.stryker.com/",
                 "Employer Phone": "",
-                "Employer Logo": "",
-                "Company Description": "",
+                "Employer Logo": "https://d25zu39ynyitwy.cloudfront.net/oms/000000/image/2024/5/LFSKF_logo/logo_-1x-1.png",
+                "Company Description": "Stryker is one of the world's leading medical technology companies. Alongside our customers around the world, we impact more than 150 million patients annually.",
                 "Status": "Active",
             }
             JOBS.append(jobDetails)
@@ -189,6 +195,7 @@ def getJobs(driver):
         except Exception as e:
             print(f"Error in loading post details: {e}")
     return JOBS
+
 
 def scraping():
     try:
@@ -203,5 +210,6 @@ def scraping():
             print(f"Error : {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
+
 
 scraping()
